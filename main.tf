4afe7d67e6cbae96bbc988f3ac99857ce32a3a93
mina shi <mina.shi@umbrellar.com>
2022-06-30 10:59:55 +1200

    u

--- main.tf
+++ main.tf
@@ -5,16 +5,16 @@ provider "azurerm" {
   #  client_id       = "185d5555-e433-4286-b619-9afc9430f42d"
   #  client_secret   = "ceylvAVDtkLE4XrIvbH0.A15MT-61f1kxg"
   #  tenant_id       = "9ff41cc1-b5be-4a5e-8b1e-cea44886ca5a"
-  subscription_id = var.AZURE_SUBSCRIPTION_ID
-  client_id       = var.AZURE_CLIENT_ID
-  client_secret   = var.AZURE_SECRET_ID
-  tenant_id       = var.AZURE_TENANT_ID
+  subscription_id = var.subscription-id
+  client_id       = var.client-id
+  client_secret   = var.client-secret
+  tenant_id       = var.tenant-id
 }
 resource "azurerm_resource_group" "minatest" {
   name     = "minagithub"
   location = "West Europe"
 }
-variable "AZURE_SUBSCRIPTION_ID" {}
-variable "AZURE_CLIENT_ID" {}
-variable "AZURE_SECRET_ID" {}
-variable "AZURE_TENANT_ID" {}
+variable "subscription-id" {}
+variable "client-id" {}
+variable "client-secret" {}
+variable "tenant-id" {}
