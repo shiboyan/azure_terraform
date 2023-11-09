0a758088ef3c0de87a3553973df440bc3028e270
minashi <aminah.xy@gmail.com>
2023-11-09 15:09:29 +1300

    test master

--- main.tf
+++ main.tf
@@ -1,14 +1,6 @@
 provider "azurerm" {
   features {}
 
-  #  subscription_id = "1f8377a3-e548-4aae-a516-6a1a644c434c"
-  #  client_id       = "185d5555-e433-4286-b619-9afc9430f42d"
-  #  client_secret   = "ceylvAVDtkLE4XrIvbH0.A15MT-61f1kxg"
-  #  tenant_id       = "9ff41cc1-b5be-4a5e-8b1e-cea44886ca5a"
-  #  subscription_id = var.subscription-id
-  #  client_id       = var.client-id
-  #  client_secret   = var.client-secret
-  #  tenant_id       = var.tenant-id
 }
 resource "azurerm_resource_group" "minatest" {
   name     = "minagithub"
